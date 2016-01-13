fd41b07367837909a2c8279bdefbe029a4bb2407
Michael Teagle <mkteagle@gmail.com>
2016-01-13 10:24:27 -0700

    just uploading some changes

--- portfolio.new/todo/app/app.js
+++ portfolio.new/todo/app/app.js
@@ -1,10 +1,97 @@
 (function () {
     'use strict';
+    var translations = {
+        Archived: 'Archive List',
+        Items: 'Archive Items',
+        Delete: 'Delete List',
+        Contact: 'Contact Me',
+        Copyright: 'Copyright',
+        ToDo: 'New ToDo...',
+        List: 'New List...',
+        AI: 'Archived Items',
+        About: 'About My App',
+        EnglishBu: 'English',
+        FrenchBu: 'français',
+        GermanBu: 'Deutsche',
+        SpanishBu: 'Español',
+        NoItems: 'No Items in this List',
+        Unarchive: 'Unarchive Item(s)',
+        HomeBu: 'Home',
+        AboutBu: 'About',
+        ArchiveBu: 'Archived'
+    };
+    var translationsDE = {
+        Archived: 'archivieren Liste',
+        Items: 'Archiv Artikel',
+        Delete: 'Löschen Liste',
+        Contact: 'Kontaktiere mich',
+        Copyright: 'Copyright',
+        ToDo: 'Neu ToDo...',
+        List: 'neue Liste...',
+        AI: 'Archivierte Artikel',
+        About: 'Über Meine App',
+        EnglishBu: 'English',
+        FrenchBu: 'français',
+        GermanBu: 'Deutsche',
+        SpanishBu: 'Español',
+        NoItems: 'Keine Einträge in dieser Liste',
+        Unarchive: 'Dearchivieren(r) Artikel',
+        HomeBu: 'Zuhause',
+        AboutBu: 'Etwa',
+        ArchiveBu: 'archiviert'
+    };
+    var translationsFR = {
+        Archived: 'Les Archives liste',
+        Items: 'Les Archives Articles',
+        Delete: 'Effacer liste',
+        Contact: 'Le Contact Moi',
+        Copyright: 'droits dauteur',
+        ToDo: 'Nouveau ToDo...',
+        List: 'nouvelle liste...',
+        AI: 'Articles archivés',
+        About: 'A propos de My App',
+        EnglishBu: 'English',
+        FrenchBu: 'français',
+        GermanBu: 'Deutsche',
+        SpanishBu: 'Español',
+        NoItems: "Pas d'éléments dans cette liste",
+        Unarchive: 'Désarchiver article(s)',
+        HomeBu: 'Accueil',
+        AboutBu: 'sur',
+        ArchiveBu: 'archivé'
+    };
+    var translationsES = {
+        Archived: 'Archivo lista',
+        Items: 'Archivo de Artículos',
+        Delete: 'Borrar lista',
+        Contact: 'Contáctame',
+        Copyright: 'Derechos de autor',
+        ToDo: 'Nueva ToDo...',
+        List: 'Nueva lista...',
+        AI: 'Contenido archivado',
+        About: 'Acerca de Mi App',
+        EnglishBu: 'English',
+        FrenchBu: 'français',
+        GermanBu: 'Deutsche',
+        SpanishBu: 'Español',
+        NoItems: 'No hay artículos en esta lista',
+        Unarchive: 'Artículo Desarchivar(s)',
+        HomeBu: 'Casa',
+        AboutBu: 'Acerca de',
+        ArchiveBu: 'archivado'
+    };
     angular
-        .module('starterApp', ['ngMaterial', 'ui.router', 'listService', 'navController', 'listDirectives', 'listFilter', 'listController'])
-        .config(["$stateProvider", "$urlRouterProvider", "$mdThemingProvider", "$mdIconProvider",
-            function ($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider) {
-
+        .module('starterApp', ['ngMaterial', 'ui.router', 'listService', 'navController', 'listDirectives', 'listFilter', 'listController', 'ngAnimate', 'pascalprecht.translate'])
+        .config(["$stateProvider", "$urlRouterProvider", "$mdThemingProvider", "$mdIconProvider", '$translateProvider',
+            function ($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider, $translateProvider) {
+                $translateProvider
+                    .translations('en', translations)
+                    .translations('de', translationsDE)
+                    .translations('fr', translationsFR)
+                    .translations('es', translationsES)
+                    .preferredLanguage('en');
+                $translateProvider.useSanitizeValueStrategy(null);
+                //$translateProvider.useLocalStorage();
                 // define all app states (pages)
                 $stateProvider
                     .state("home", {
--- portfolio.new/todo/app/src/js/navController.js
+++ portfolio.new/todo/app/src/js/navController.js
@@ -1,18 +1,23 @@
 (function(){
     'use strict';
 
-    angular.module('navController', [])
+    angular.module('navController', ["ngAnimate"])
         .controller('navController', navController);
 
-    navController.$inject = ["$location"];
+    navController.$inject = ["$location", "$translate"];
 
-    function navController($location)
+    function navController($location, $translate)
     {
         var nav = this;
         nav.isActive = isActive;
+        nav.changeLanguage = changeLanguage;
+        function changeLanguage (langKey) {
+            $translate.use(langKey);
+        }
         function isActive(viewLocation) {
             return viewLocation === $location.path();
         }
+
     }
 
 }());
