ca1fd5649d6fc3e2f9e9d1f02c58bce4e5707cae
Michael Teagle <mkteagle@gmail.com>
2016-01-27 06:57:17 -0700

    trying to fix timesheet to work properly

--- portfolio.new/timesheet/app.js
+++ portfolio.new/timesheet/app.js
@@ -3,42 +3,54 @@ angular.module('timeSheet', [])
 //timeSheetController.$inject = ['tsService'];
 function timeSheetController () {
     var self = this;
+    self.firstime = 0;
+    self.secondtime = 0;
+    self.thirdtime = 0;
+    self.total = 0;
+    self.finalCalc = 0;
+    self.h = 0;
+    self.m = 0;
+    self.s = 0;
+    self.hours = 0;
+    self.mins = 0;
+    self.secs = 0;
     self.getFirstTime = getFirstTime;
     self.getSecondTime = getSecondTime;
     self.getThirdTime = getThirdTime;
     self.totals = totals;
-    self.timeConverter = timeConverter;
+    self.finale = finale;
     function getFirstTime() {
-        console.log(timeConverter(1453787100));
-        self.firsttime = new Date(timeConverter(1453787100));
+        var date = new Date();
+        self.firsttime = Math.floor((date.getTime() / 1000) % 60);
     }
     function getSecondTime() {
-        var timee = Date.now();
-        console.log(timeConverter(1453792505));
-        self.secondtime = new Date(timeConverter(1453792505));
+        var date = new Date();
+        self.secondtime = Math.floor((date.getTime() / 1000) % 60);
+        totals();
     }
     function getThirdTime() {
-        self.thirdtime = new Date();
-        var third = self.thirdtime;
-        totals(third);
+        var date = new Date();
+        self.thirdtime = Math.floor((date.getTime() / 1000) % 60);
+        finale();
     }
-    function totals(time) {
-        var total2 = self.firsttime - self.secondtime;
-        console.log(total2);
-        self.total = time.setHours(time.getHours() + 8 - total2);
+    function totals() {
+        self.eight = 28800;
+        self.total = self.eight - (self.secondtime - self.firsttime);
+        var d = Number(self.total);
+        self.h = Math.floor(d / 3600);
+        self.m = Math.floor(d % 3600 / 60);
+        self.s = Math.floor(d % 3600 % 60);
+        var date = new Date();
+        self.hours = (date.getHours() + self.h);
+        self.mins = (date.getMinutes() + self.m);
+        self.secs = (date.getSeconds() + self.s);
+        self.total = (self.hours + ":" + self.mins + ":" + self.secs);
     }
-    function timeConverter(UNIX_timestamp){
-        var time;
-        var a = new Date(UNIX_timestamp*1000);
-        var hour = a.getUTCHours();
-        var min = a.getUTCMinutes();
-        if (hour < 10) {
-            time = '0'+hour+':'+min;
-            return time;
-        }
-        else {
-            time = hour+':'+min;
-            return time;
-        }
+    function finale() {
+        var date = new Date();
+        self.hours = (date.getHours() + self.h);
+        self.mins = (date.getMinutes() + self.m);
+        self.secs = (date.getSeconds() + self.s);
+        self.finalCalc = (self.hours + ":" + self.mins + ":" + self.secs);
     }
 }
